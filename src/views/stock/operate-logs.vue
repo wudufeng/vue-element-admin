97958aa0430f0393ce6f4b38bf88ee50fa355bb7
wudf@UEB1072 <wudufeng@uebcorp.com>
2019-07-31 14:40:30 +0800

    more

--- src/views/stock/operate-logs.vue
+++ src/views/stock/operate-logs.vue
@@ -3,10 +3,13 @@
     <div class="filter-container">
       <el-row>
         <el-col :span="2">
-          <PlatformCode v-model="listQuery.platformCode" />
+          <PlatformCode v-model="listQuery.platformCode" style="width: 120px;" />
         </el-col>
         <el-col :span="3">
-          <el-input v-model="listQuery.executionId" placeholder="执行批次号" style="width: 180px;" class="filter-item" />
+          <el-input v-model="listQuery.executionId" placeholder="执行批次号" style="width: 180px;padding-left: 10px" class="filter-item" />
+        </el-col>
+        <el-col :span="2">
+          <el-input v-model="listQuery.accountId" placeholder="账号" style="width: 120px;" class="filter-item" />
         </el-col>
         <el-col :span="2">
           <el-input v-model="listQuery.sku" placeholder="sku" style="width: 120px;" class="filter-item" />
