3c43ed4e993a53cc0cbabfb922711440352eba3d
wudf@UEB1072 <wudufeng@uebcorp.com>
2019-11-14 15:17:36 +0800

    add

--- src/views/order/trigger-pull.vue
+++ src/views/order/trigger-pull.vue
@@ -2,7 +2,7 @@
   <div class="app-container">
     <el-row>
       <el-col :span="18">
-        <avue-form v-model="data" :option="option" @submit="handleSubmit" />
+        <avue-form ref="avueForm" v-model="data" :option="option" @submit="handleSubmit" />
       </el-col>
     </el-row>
   </div>
@@ -25,7 +25,7 @@ export default {
         labelWidth: '238',
         labelPosition: 'right',
         column: [
-          { label: '平台编码', prop: 'platformCode', type: 'select', rules: [{ required: true, message: '内部平台编码不能为空', trigger: 'blur' }], dicData: [{ label: 'SHOPEE', value: 'SHOPEE' }, { label: '乐天', value: 'rakuten' }, { label: 'FLIPKART', value: 'Flipkart' }] },
+          { label: '平台编码', prop: 'platformCode', type: 'select', rules: [{ required: true, message: '内部平台编码不能为空', trigger: 'blur' }], dicData: [{ label: 'SHOPEE', value: 'SHOPEE' }, { label: 'RAKUTEN', value: 'rakuten' }, { label: 'FLIPKART', value: 'Flipkart' }] },
           { label: '账号ID', prop: 'accountId', rules: [{ required: true, message: '账号ID不能为空', trigger: 'blur' }] },
           { label: '开始时间', prop: 'queryBeginTime', type: 'datetime', valueFormat: 'yyyyMMddHHmmss' },
           { label: '结束时间', prop: 'queryEndTime', type: 'datetime', valueFormat: 'yyyyMMddHHmmss' },
@@ -33,6 +33,17 @@ export default {
           { label: '额外请求参数', prop: 'extraJSON', type: 'text', span: 24 }
           // { label: '刷新策略', prop: 'compareUseUpdateTime', rules: [{ required: true, message: '刷新策略不能为空', trigger: 'blur' }], type: 'select', dicData: [{ value: true, label: '判断平台更新时间' }, { value: false, label: '对比字段' }] }
         ]
+      }
+    }
+  },
+  watch: {
+    'data.platformCode': {
+      handler(newVal, oldVal) {
+        if (newVal === 'rakuten') {
+          this.data.extraJSON = '[{"dateType":"3"},{"dateType":"4"},{"dateType":"5"},{"dateType":"6"}]'
+        } else {
+          this.data.extraJSON = ''
+        }
       }
     }
   },
